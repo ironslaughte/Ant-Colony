c2f9b013ac7d82b78b151de9c86a4017f07edf3d
ironslaughte <58146305+ironslaughte@users.noreply.github.com>
2022-12-10 15:56:43 +0300

    Add files via upload

---
+++ AntColony/AntColony.cs
@@ -0,0 +1,318 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AntColony
+{
+    public class AntColony
+    {
+        private int _numCities;
+        private int _numAnts;
+        private int _alpha;
+        private int _beta;
+        private double _bestLength, _rho, Q;
+
+        private int[] _bestTrail;
+        private int[][] _dists;
+        private int[][] _ants;
+        private double[][] _pheromones;
+        private static Random random = new Random(0);
+
+        private const int MaxTime = 1000;
+
+
+        public AntColony(int numCities, int numAnts, int alpha, int beta, double Q, double rho)
+        {
+            _numCities = numCities;
+            _numAnts = numAnts;
+            _alpha = alpha;
+            _beta = beta;
+            _rho = rho;
+            this.Q = Q;
+            InitializeComponents();
+        }
+
+        private void InitializeComponents()
+        {
+            _dists = MakeGraphDistances(_numCities);
+            _ants = InitAnts();
+            _pheromones = InitPheromones();
+        }
+
+        public IEnumerable<double> Run()
+        {
+                _bestTrail = BestTrail();
+                _bestLength = Length(_bestTrail);
+
+                int time = 0;
+                while (time < MaxTime)
+                {
+                    UpdateAnts();
+                    UpdatePheromones();
+
+                    int[] curr_bestTrail = BestTrail();
+                    double curr_bestLength = Length(curr_bestTrail);
+                    if (curr_bestLength < _bestLength)
+                    {
+                        _bestLength = curr_bestLength;
+                        _bestTrail = curr_bestTrail;
+                        yield return _bestLength;
+                    }
+                    time += 1;
+                }
+        }
+
+        public (double, int[]) GetBest() => (_bestLength, _bestTrail);
+
+        public int[][] GetGraph() => _dists;
+        private  int[][] InitAnts()
+        {
+            int[][] ants = new int[_numAnts][];
+            for (int k = 0; k <= _numAnts - 1; k++)
+            {
+                int start = random.Next(0, _numCities);
+                ants[k] = RandomTrail(start);
+            }
+            return ants;
+        }
+
+        private  int[] RandomTrail(int start)
+        {
+            int[] trail = new int[_numCities];
+
+            for (int i = 0; i <= _numCities - 1; i++)
+                trail[i] = i;
+
+            for (int i = 0; i <= _numCities - 1; i++)
+            {
+                int r = random.Next(i, _numCities);
+                int tmp = trail[r];
+                trail[r] = trail[i];
+                trail[i] = tmp;
+            }
+
+            int idx = IndexOfTarget(trail, start);
+            int temp = trail[0];
+            trail[0] = trail[idx];
+            trail[idx] = temp;
+
+            return trail;
+        }
+
+        private  int IndexOfTarget(int[] trail, int target)
+        {
+            for (int i = 0; i <= trail.Length - 1; i++)
+                if (trail[i] == target)
+                    return i;
+
+            throw new Exception("Target not found in IndexOfTarget");
+        }
+
+        private  double Length(int[] trail)
+        {
+            double result = 0.0;
+            for (int i = 0; i <= trail.Length - 2; i++)
+                result += Distance(trail[i], trail[i + 1]);
+            result += Distance(trail[0], trail[trail.Length - 1]);
+            return result;
+        }
+
+        private  int[] BestTrail()
+        {
+            double _bestLength = Length(_ants[0]);
+            int idx_bestLength = 0;
+            for (int k = 1; k <= _ants.Length - 1; k++)
+            {
+                double len = Length(_ants[k]);
+                if (len < _bestLength)
+                {
+                    _bestLength = len;
+                    idx_bestLength = k;
+                }
+            }
+            int _numCities = _ants[0].Length;           
+            int[] _bestTrail_Renamed = new int[_numCities];
+            _ants[idx_bestLength].CopyTo(_bestTrail_Renamed, 0);
+            return _bestTrail_Renamed;
+        }
+
+        private  double[][] InitPheromones()
+        {
+            double[][] pheromones = new double[_numCities][];
+            for (int i = 0; i <= _numCities - 1; i++)
+                pheromones[i] = new double[_numCities];
+
+            for (int i = 0; i <= pheromones.Length - 1; i++)
+            {
+                for (int j = 0; j <= pheromones[i].Length - 1; j++)
+                    pheromones[i][j] = 0.01;
+                    
+            }
+            return pheromones;
+        }
+
+        private  void UpdateAnts()
+        {           
+            for (int k = 0; k <= _ants.Length - 1; k++)
+            {
+                int start = random.Next(0, _numCities);
+                int[] newTrail = BuildTrail(k, start);
+                _ants[k] = newTrail;
+            }
+        }
+
+        private  int[] BuildTrail(int k, int start)
+        {          
+            int[] trail = new int[_numCities];
+            bool[] visited = new bool[_numCities];
+            trail[0] = start;
+            visited[start] = true;
+            for (int i = 0; i <= _numCities - 2; i++)
+            {
+                int cityX = trail[i];
+                int next = NextCity(k, cityX, visited);
+                trail[i + 1] = next;
+                visited[next] = true;
+            }
+            return trail;
+        }
+
+        private  int NextCity(int k, int cityX, bool[] visited)
+        {
+            double[] probs = MoveProbs(cityX, visited);
+
+            double[] cumul = new double[probs.Length + 1];
+            for (int i = 0; i <= probs.Length - 1; i++)
+                cumul[i + 1] = cumul[i] + probs[i];
+
+            double p = random.NextDouble();
+
+            for (int i = 0; i <= cumul.Length - 2; i++)
+                if (p >= cumul[i] && p < cumul[i + 1])
+                    return i;
+
+            throw new Exception("Failure to return valid city in NextCity");
+        }
+
+        private  double[] MoveProbs(int cityX, bool[] visited)
+        {
+            double[] taueta = new double[_numCities];
+            double sum = 0.0;
+            for (int i = 0; i <= taueta.Length - 1; i++)
+            {
+                if (i == cityX)
+                    taueta[i] = 0.0;
+                else if (visited[i] == true)
+                    taueta[i] = 0.0;    
+                else
+                {
+                    taueta[i] = Math.Pow(_pheromones[cityX][i], _alpha) * Math.Pow((1.0 / Distance(cityX, i)), _beta);
+                    if (taueta[i] < 0.0001)
+                        taueta[i] = 0.0001;
+                    else if (taueta[i] > (double.MaxValue / (_numCities * 100)))
+                        taueta[i] = double.MaxValue / (_numCities * 100);
+                }
+                sum += taueta[i];
+            }
+
+            double[] probs = new double[_numCities];
+            for (int i = 0; i <= probs.Length - 1; i++)
+                probs[i] = taueta[i] / sum;
+            return probs;
+        }
+
+        private  void UpdatePheromones()
+        {
+            for (int i = 0; i <= _pheromones.Length - 1; i++)
+            {
+                for (int j = i + 1; j <= _pheromones[i].Length - 1; j++)
+                {
+                    for (int k = 0; k <= _ants.Length - 1; k++)
+                    {
+                        double length = Length(_ants[k]);
+                        // length of ant k trail
+                        double decrease = (1.0 - _rho) * _pheromones[i][j];
+                        double increase = 0.0;
+                        if (EdgeInTrail(i, j, _ants[k]) == true)
+                            increase = (Q / length);
+
+                        _pheromones[i][j] = decrease + increase;
+
+                        if (_pheromones[i][j] < 0.0001)
+                            _pheromones[i][j] = 0.0001;
+                        else if (_pheromones[i][j] > 100000.0)
+                            _pheromones[i][j] = 100000.0;
+
+                        _pheromones[j][i] = _pheromones[i][j];
+                    }
+                }
+            }
+        }
+
+        private  bool EdgeInTrail(int cityX, int cityY, int[] trail)
+        {
+            int lastIndex = trail.Length - 1;
+            int idx = IndexOfTarget(trail, cityX);
+
+            if (idx == 0 && trail[1] == cityY)
+            {
+                return true;
+            }
+            else if (idx == 0 && trail[lastIndex] == cityY)
+            {
+                return true;
+            }
+            else if (idx == 0)
+            {
+                return false;
+            }
+            else if (idx == lastIndex && trail[lastIndex - 1] == cityY)
+            {
+                return true;
+            }
+            else if (idx == lastIndex && trail[0] == cityY)
+            {
+                return true;
+            }
+            else if (idx == lastIndex)
+            {
+                return false;
+            }
+            else if (trail[idx - 1] == cityY)
+            {
+                return true;
+            }
+            else if (trail[idx + 1] == cityY)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        private  int[][] MakeGraphDistances(int _numCities)
+        {
+            int[][] dists = new int[_numCities][];
+            for (int i = 0; i <= dists.Length - 1; i++)
+                 dists[i] = new int[_numCities];
+            for (int i = 0; i <= _numCities - 1; i++)
+            {
+                for (int j = i + 1; j <= _numCities - 1; j++)
+                {
+                    int d = random.Next(1, 9);
+                    // [1,8]
+                    dists[i][j] = d;
+                    dists[j][i] = d;
+                }
+            }
+            return dists;
+        }
+
+        private double Distance(int cityX, int cityY) => _dists[cityX][cityY];
+    }
+}
+
---
+++ AntColony/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace AntColony
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxNumCities = new System.Windows.Forms.TextBox();
+            this.textBoxNumAnts = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxAlpha = new System.Windows.Forms.TextBox();
+            this.Alpha = new System.Windows.Forms.Label();
+            this.textBoxBeta = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxQ = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.Answer = new System.Windows.Forms.Label();
+            this.textBoxRho = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.labelCurrentBestLen = new System.Windows.Forms.Label();
+            this.richTextBoxResAlg = new System.Windows.Forms.RichTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.labelGraph = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Количество городов";
+            // 
+            // textBoxNumCities
+            // 
+            this.textBoxNumCities.Location = new System.Drawing.Point(154, 29);
+            this.textBoxNumCities.Name = "textBoxNumCities";
+            this.textBoxNumCities.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNumCities.TabIndex = 1;
+            this.textBoxNumCities.Text = "60";
+            // 
+            // textBoxNumAnts
+            // 
+            this.textBoxNumAnts.Location = new System.Drawing.Point(154, 55);
+            this.textBoxNumAnts.Name = "textBoxNumAnts";
+            this.textBoxNumAnts.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNumAnts.TabIndex = 3;
+            this.textBoxNumAnts.Text = "4";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Количество муравьев";
+            // 
+            // textBoxAlpha
+            // 
+            this.textBoxAlpha.Location = new System.Drawing.Point(154, 81);
+            this.textBoxAlpha.Name = "textBoxAlpha";
+            this.textBoxAlpha.Size = new System.Drawing.Size(100, 20);
+            this.textBoxAlpha.TabIndex = 5;
+            this.textBoxAlpha.Text = "3";
+            // 
+            // Alpha
+            // 
+            this.Alpha.AutoSize = true;
+            this.Alpha.Location = new System.Drawing.Point(24, 84);
+            this.Alpha.Name = "Alpha";
+            this.Alpha.Size = new System.Drawing.Size(34, 13);
+            this.Alpha.TabIndex = 4;
+            this.Alpha.Text = "Alpha";
+            // 
+            // textBoxBeta
+            // 
+            this.textBoxBeta.Location = new System.Drawing.Point(154, 107);
+            this.textBoxBeta.Name = "textBoxBeta";
+            this.textBoxBeta.Size = new System.Drawing.Size(100, 20);
+            this.textBoxBeta.TabIndex = 7;
+            this.textBoxBeta.Text = "2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Beta";
+            // 
+            // textBoxQ
+            // 
+            this.textBoxQ.Location = new System.Drawing.Point(154, 133);
+            this.textBoxQ.Name = "textBoxQ";
+            this.textBoxQ.Size = new System.Drawing.Size(100, 20);
+            this.textBoxQ.TabIndex = 9;
+            this.textBoxQ.Text = "2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 136);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(15, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Q";
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(91, 185);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(75, 23);
+            this.StartButton.TabIndex = 10;
+            this.StartButton.Text = "Запустить";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // Answer
+            // 
+            this.Answer.AutoSize = true;
+            this.Answer.Location = new System.Drawing.Point(350, 185);
+            this.Answer.Name = "Answer";
+            this.Answer.Size = new System.Drawing.Size(0, 13);
+            this.Answer.TabIndex = 11;
+            // 
+            // textBoxRho
+            // 
+            this.textBoxRho.Location = new System.Drawing.Point(154, 159);
+            this.textBoxRho.Name = "textBoxRho";
+            this.textBoxRho.Size = new System.Drawing.Size(100, 20);
+            this.textBoxRho.TabIndex = 13;
+            this.textBoxRho.Text = "0,01";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(24, 162);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(22, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "rho";
+            // 
+            // labelCurrentBestLen
+            // 
+            this.labelCurrentBestLen.AutoSize = true;
+            this.labelCurrentBestLen.Location = new System.Drawing.Point(343, 32);
+            this.labelCurrentBestLen.Name = "labelCurrentBestLen";
+            this.labelCurrentBestLen.Size = new System.Drawing.Size(0, 13);
+            this.labelCurrentBestLen.TabIndex = 14;
+            // 
+            // richTextBoxResAlg
+            // 
+            this.richTextBoxResAlg.Location = new System.Drawing.Point(300, 29);
+            this.richTextBoxResAlg.Name = "richTextBoxResAlg";
+            this.richTextBoxResAlg.Size = new System.Drawing.Size(488, 409);
+            this.richTextBoxResAlg.TabIndex = 15;
+            this.richTextBoxResAlg.Text = "";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 238);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(148, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Матрциа смежности графа:";
+            // 
+            // labelGraph
+            // 
+            this.labelGraph.AutoSize = true;
+            this.labelGraph.Location = new System.Drawing.Point(13, 255);
+            this.labelGraph.Name = "labelGraph";
+            this.labelGraph.Size = new System.Drawing.Size(0, 13);
+            this.labelGraph.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(795, 514);
+            this.Controls.Add(this.labelGraph);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.richTextBoxResAlg);
+            this.Controls.Add(this.labelCurrentBestLen);
+            this.Controls.Add(this.textBoxRho);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.Answer);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.textBoxQ);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxBeta);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxAlpha);
+            this.Controls.Add(this.Alpha);
+            this.Controls.Add(this.textBoxNumAnts);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxNumCities);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxNumCities;
+        private System.Windows.Forms.TextBox textBoxNumAnts;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxAlpha;
+        private System.Windows.Forms.Label Alpha;
+        private System.Windows.Forms.TextBox textBoxBeta;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxQ;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Label Answer;
+        private System.Windows.Forms.TextBox textBoxRho;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label labelCurrentBestLen;
+        private System.Windows.Forms.RichTextBox richTextBoxResAlg;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label labelGraph;
+    }
+}
+
